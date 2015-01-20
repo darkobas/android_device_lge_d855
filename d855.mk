cbb19887a8d9f8bb57e039b1ff5fac5d0c3bae3c
AngelBob <cambercreak@gmail.com>
2015-01-20 15:42:01 +0100

    update fingerprint

--- d855.mk
+++ d855.mk
@@ -58,8 +58,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1417604035/1417604035:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1417604035 1417604035 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1419207951/1419207951:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1419207951 1419207951 release-keys" 
 
 # common g3
 $(call inherit-product, device/lge/g3-common/g3.mk)
