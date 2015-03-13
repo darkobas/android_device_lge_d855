24aa25150ca77464cd41b2ce327e9d364b0fbff0
darkobas <darkobas@gmail.com>
2015-03-13 10:52:29 +0100

    change name

--- d855.mk
+++ d855.mk
@@ -57,7 +57,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_global_com" \
+    PRODUCT_NAME="d855" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1419207951/1419207951:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1419207951 1419207951 release-keys" 
 
